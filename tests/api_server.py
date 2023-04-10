70495badd4ea4403d199d81bb981ad1cb11d33f9
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-04-10 11:45:35 +0200

    feat: simple test

--- tests/api_server.py
+++ tests/api_server.py
@@ -1,11 +1,16 @@
 import os
 from pathlib import Path
-from node_chatgpt_api import BingAIClient, BingMessage, start_api_server
+from node_chatgpt_api import BingAIClient, BingMessage, start_api_server, stop_api_server
+from util import enable_http_logs
 
-start_api_server(os.path.join(Path(os.getcwd()).parent.absolute(), "settings.js"))
+#start_api_server(os.path.join(Path(os.getcwd()).parent.absolute(), "settings.js"))
 
-client = BingAIClient()
+enable_http_logs()
+
+client = BingAIClient(jailbreakConversationId=True)
 response = client.ask(BingMessage("How are you?"))
 
 print(response.error)
 print(response.response)
+
+#stop_api_server()
